f6e11dc598b9cc64ac94183e8d2b7d611258fd83
muhtarA <muhtarrn@gmail.com>
2018-03-19 03:36:03 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import SearchBar from './components/SearchBar'
+
+const App = () => {
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    )
+}
+
+export default App;
